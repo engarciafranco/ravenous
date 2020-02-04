ddb3e5163011ea0c35a7e2d2bce6283bf14ec01a
Enrique Garcia <enrique@codestreamstudios.com>
2020-02-04 15:04:41 -0600

    [Components] components were prepared for use with the Yelp API. business object was moved to App.js and a businesses array was created

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,12 +6,11 @@ class BusinessList extends React.Component {
     render() {
         return(
             <div className="BusinessList">
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
+                {
+                    this.props.businesses.map(function(business) {  //business => { ... } also works!
+                        return <Business business={business}/>;
+                })
+                }
             </div>
         )
     }
